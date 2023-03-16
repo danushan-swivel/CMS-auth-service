9d805a0481515cbc0dda216f575b75fa4be48a23
DanushanKanagasingam <danushan@swivelgroup.com.au>
2023-03-16 22:47:59 +0530

    added

--- src/main/java/com/cms/auth/CmsAuthServiceApplication.java
+++ src/main/java/com/cms/auth/CmsAuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.cms.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CmsAuthServiceApplication {
 
 	public static void main(String[] args) {
