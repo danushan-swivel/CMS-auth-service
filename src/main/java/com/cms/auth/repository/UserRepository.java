968c0a650388b65249cde1731da7eba4e84d5881
DanushanKanagasingam <danushan@swivelgroup.com.au>
2023-03-17 20:20:09 +0530

    added

--- src/main/java/com/cms/auth/repository/UserRepository.java
+++ src/main/java/com/cms/auth/repository/UserRepository.java
@@ -14,7 +14,7 @@ public interface UserRepository extends JpaRepository<User, String> {
     @Query(value = "SELECT * FROM user u WHERE u.is_deleted=false AND u.user_name =?1", nativeQuery = true)
     User findByUserName(String userName);
 
-    @Query(value = "SELECT COUNT(u.user_name) FROM user u WHERE u.is_deleted=false AND u.user_name =?1", nativeQuery = true)
+//    @Query(value = "SELECT COUNT(u.user_name) FROM user u WHERE u.is_deleted=false AND u.user_name =?1", nativeQuery = true)
     boolean existsByUserNameAndIsDeletedFalse(String userName);
 
     @Query(value = "SELECT * FROM user u WHERE u.is_deleted=false", nativeQuery = true)
